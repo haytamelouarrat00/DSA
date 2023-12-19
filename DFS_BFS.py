2e8ab3430ed55e027850b0b03f908d201a3feeaf
haytamelouarrat00 <eo.haytam@gmil.com>
2023-12-19 17:42:10 +0100

    commit 19/12/23 created new file for BFS and DFS

--- DFS_BFS.py
+++ DFS_BFS.py
@@ -1,69 +1,53 @@
-class Graph:
+from Graph import *
+def BFS(Graph, start):
+    queue = [start]
+    processed = []
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in processed:
+            processed.append(vertex)
+            queue.extend(neighbor for neighbor in Graph.get_neighbors(vertex) if neighbor not in processed)
 
-    def __init__(self, directed=False):
-        self.is_directed = directed
-        self.graph = {}
-        self.node_weights = {}
-        self.visited = []
-        self.unvisited = {}
-        self.distances = {}
-        self.previous = {}
-
-    def add_node(self, node, weight=1):
-        self.graph[node] = {}
-        self.node_weights[node] = weight
-
-    def add_edge(self, start, end):
-        self.graph[start][end] = True
-
-        if not self.is_directed:
-            self.graph[end][start] = True
-
-    def get_neighbors(self, vertex):
-        return list(self.graph.get(vertex, {}).keys())
-
-    def get_weight(self, vertex):
-        return self.graph[vertex]
-
-    def get_successors(self, vertex):
-        return list(self.graph.get(vertex, {}).keys())
-
-    def get_predecessors(self, vertex):
-        predecessors = []
-        for v in self.graph:
-            if vertex in self.graph[v]:
-                predecessors.append(v)
-        return predecessors
-
-    def print_graph(self):
-        print(self.graph)
-
-    def BFS(self, start):
-        queue = [start]
-        processed = []
-        while queue:
-            vertex = queue.pop(0)
-            if vertex not in processed:
-                processed.append(vertex)
-                queue.extend(self.get_successors(vertex))
-                queue.sort()
-        return processed
+    return processed
 
 
-g = Graph(directed=True)
-g.add_node('A', 5)
-g.add_node('B', 10)
-g.add_node('C', 7)
-g.add_node('D', 3)
-g.add_node('E', 2)
-g.add_node('F', 1)
+# Create a directed graph
+graph_directed = Graph(directed=True)
 
-g.add_edge('A', 'B')
-g.add_edge('B', 'C')
-g.add_edge('B', 'E')
-g.add_edge('A', 'D')
-g.add_edge('D', 'F')
+# Add nodes with weights
+graph_directed.add_node('A', 5)
+graph_directed.add_node('B', 10)
+graph_directed.add_node('C', 7)
+graph_directed.add_node('D', 3)
+graph_directed.add_node('E', 2)
+graph_directed.add_node('F', 1)
 
-print(g.BFS('A'))
+# Add edges
+graph_directed.add_edge('A', 'B')
+graph_directed.add_edge('B', 'C')
+graph_directed.add_edge('B', 'E')
+graph_directed.add_edge('A', 'D')
+graph_directed.add_edge('D', 'F')
 
-g.print_graph()
+# Test BFS on directed graph
+print(BFS(graph_directed, 'A'))
+# Expected output: ['A', 'B', 'D', 'C', 'E', 'F']
+
+# Create an undirected graph
+graph_undirected = Graph(directed=False)
+
+# Add nodes with weights
+graph_undirected.add_node('X', 3)
+graph_undirected.add_node('Y', 8)
+graph_undirected.add_node('Z', 4)
+
+# Add edges
+graph_undirected.add_edge('X', 'Y')
+graph_undirected.add_edge('Y', 'Z')
+
+# Test BFS on undirected graph
+print(BFS(graph_directed, 'X'))
+# Expected output: ['X', 'Y', 'Z']
+
+
+# g.print_graph()
---
+++ Graph.py
@@ -0,0 +1,22 @@
+class Graph:
+    def __init__(self, directed=False):
+        self.is_directed = directed
+        self.graph = {}
+        self.node_weights = {}
+        self.visited = {}
+
+    def add_node(self, node, weight=1):
+        self.graph[node] = {}
+        self.node_weights[node] = weight
+
+    def add_edge(self, start, end):
+        self.graph[start][end] = True
+
+        if not self.is_directed:
+            self.graph[end][start] = True
+
+    def get_neighbors(self, vertex):
+        return list(self.graph.get(vertex, {}).keys())
+
+    def get_weight(self, vertex):
+        return self.node_weights[vertex]
